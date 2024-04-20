6b1fb70e8ce869684b8b2cdcc4f672df4f9352ca
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-04-21 07:37:50 +0800

    Add files via upload

---
+++ scriptbutton.js
@@ -0,0 +1,12 @@
+/** 
+ * Listen for click event across the document & trigger on
+ * buttons that have a data-url attribute.
+ */
+document.addEventListener('click', function (event) {
+	if (event.target.matches('[data-prototype-url]')) {
+    var button = event.target;
+		var buttonURL = button.getAttribute('data-prototype-url');
+    window.location.href = buttonURL;
+    console.log(button.textContent + ' clicked');
+	}
+}, false);
